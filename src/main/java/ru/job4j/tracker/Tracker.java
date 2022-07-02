ef8662eae62a9fb40f757a68610639c2acb3074e
gilvat <gilvat13@gmail.com>
2022-07-02 21:55:28 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -42,15 +42,27 @@ public class Tracker {
         return rsl;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        boolean rsl;
+        int index = indexOf(id);
+        items[index].setId(id);
+        items[index].setName(item.getName());
+        return (index >= 0);
     }
 }
