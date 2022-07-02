b726ef40c0bb9e4c9fd62406756897a09aa38ca5
gilvat <gilvat13@gmail.com>
2022-07-02 22:06:20 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -65,4 +65,14 @@ public class Tracker {
         items[index].setName(item.getName());
         return (index >= 0);
     }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        int start = index + 1;
+        int length = size - index - 1;
+        System.arraycopy(items, start, items, index, length);
+        items[size - 1] = null;
+        size--;
+        return (index >= 0);
+    }
 }
