7b1e6851b4c8ea489c1f693fbad5962952125b5a
gilvat <gilvat13@gmail.com>
2022-07-17 23:27:29 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passanger(int count) {
+
+    }
+
+    @Override
+    public double fuel(int fuelCount) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,9 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void drive();
+
+    void passanger(int count);
+
+    double fuel(int fuelCount);
+}
