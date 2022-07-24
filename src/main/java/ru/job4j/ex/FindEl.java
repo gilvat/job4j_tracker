cfeb27faf88ac90d8b972b5eb5455b2f82a5322d
gilvat <gilvat13@gmail.com>
2022-07-24 10:43:16 +0300

    0.4. Пользовательские исключения. [#219367 #325254]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -6,12 +6,18 @@ public class FindEl {
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 rsl = i;
+                break;
             }
         }
         if (rsl == -1) {
             throw new ElementNotFoundException("Element not found");
         }
         return rsl;
+    }
+
+    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
+        /* if contains throw ElementAbuseException */
+        return true;
     }
 
     public static void main(String[] args) {
